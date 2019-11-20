784c05a816d6bef203d0b8bdc2eca5379eb3c175
Jessica Xiao <jess@torutek.com>
2019-11-21 10:38:42 +1300

    Use Material UI instead of Bootstrap

--- Portfolio/App/src/app/index.tsx
+++ Portfolio/App/src/app/index.tsx
@@ -2,36 +2,49 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import { Route, Switch } from 'react-router';
 
-import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { amber } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 import './index.scss';
 
 import { Home } from './home';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: amber,
+	},
+	props: {
+		MuiButtonBase: {
+			disableRipple: true,
+		},
+	},
+});
+
 @observer
 export class App extends React.Component<{}, {}> {
+	
 	public render() {
 		return (
-			<>
-				<Navbar expand="lg">
-					<Navbar.Brand href="/">Jess' Portfolio</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="mr-auto" />
-						<Nav>
-							<Nav.Link href="/">About</Nav.Link>
-							<Nav.Link href="">Projects</Nav.Link>
-							<Nav.Link href="">Art</Nav.Link>
-							<Button variant="outline-primary">Contact</Button>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
+			<ThemeProvider theme={theme}>
+				<AppBar position="static" className="navbar">
+					<Toolbar>
+						<Typography variant="h6">
+							Jess' Portfolio
+						</Typography>
+						<Button>About</Button>
+						<Button>Projects</Button>
+						<Button>Art</Button>
+						<Button>Contact</Button>
+					</Toolbar>
+				</AppBar>
 				<Switch key="content">
 					<Route exact path="/" component={Home} />
 				</Switch>
-			</>
+			</ThemeProvider>
 		);
 	}
 }
