95db3affcfe45751b3c154a72e3abac1136a980c
Jessica Xiao <jess@torutek.com>
2019-12-27 16:41:40 +1300

    Add migration from last commit restructure

---
+++ Portfolio.Data/Migrations/20191227034032_AddIpAndSourceToPings.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Portfolio.Data.Migrations
+{
+    public partial class AddIpAndSourceToPings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Pings");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Pings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Pings",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Source",
+                table: "Pings",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "Pings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Pings",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Pings",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Pings",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
